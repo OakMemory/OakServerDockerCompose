1266fe33f5a6a06208193ce999e65cfa082da29a
Krysztal <suibing112233@outlook.com>
2022-08-23 01:44:47 +0800

    ✨ Feat: Disable G1GC

--- DockerFiles/paper/launch.sh
+++ DockerFiles/paper/launch.sh
@@ -11,8 +11,8 @@ if [ ! -f $EULA_FILE ]; then
     echo 'eula=true' >>$EULA_FILE
 fi
 
-if [[ "$USE_AUTHLIB_INJECT" != "false" ]]; then
-    $JAVA_PATH -server -javaagent:/paper/authlib.jar="$USE_AUTHLIB_SERVER" -Xmx"$MAX_MEM" -Xms"$MIM_MEM" "$JVM_ARGS" -jar /paper/paper.jar
+if [[ $USE_AUTHLIB_INJECT != "false" ]]; then
+    $JAVA_PATH -server -javaagent:/paper/authlib.jar="$USE_AUTHLIB_SERVER" -Xmx"$MAX_MEM" "$JVM_ARGS" -jar /paper/paper.jar
 else
-    $JAVA_PATH -server -Xmx"$MAX_MEM" -Xms"$MIM_MEM" "$JVM_ARGS" -jar /paper/paper.jar
+    $JAVA_PATH -server -Xmx"$MAX_MEM" "$JVM_ARGS" -jar /paper/paper.jar
 fi
